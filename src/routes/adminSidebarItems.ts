3fc9897c8b97e31b18e5ec4bac3ab196a1b749e3
firoz.nur <firoznur5@gmail.com>
2025-09-17 12:45:31 +0600

    Add : Overview

--- src/routes/adminSidebarItems.ts
+++ src/routes/adminSidebarItems.ts
@@ -1,5 +1,6 @@
 import AllParcels from "@/pages/Dashboard/Admin/AllParcels";
 import AllUser from "@/pages/Dashboard/Admin/AllUser";
+import Tracking from "@/pages/Dashboard/Admin/Tracking";
 import Profile from "@/pages/Dashboard/Profile";
 import { IconFolder, IconListDetails } from "@tabler/icons-react";
 import { User } from "lucide-react";
@@ -24,6 +25,12 @@ export const adminSidebarItems = [
         url: "/admin/all-parcel",
         component: AllParcels,
         icon: IconFolder,
+      },
+      {
+        title: "Overview",
+        url: "/admin/overview",
+        icon: IconListDetails,
+        component: Tracking,
       },
     ],
   },
